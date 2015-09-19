c2009f61b409b2367d2f6fb353fa80f9d83b7ab9
Tromino <idioticdinner@gmail.com>
2015-09-18 18:29:39 -0700

    Added randomized splash

---
+++ script.js
@@ -0,0 +1,34 @@
+splashes = [
+	"websites a stupid",
+	"your friendly neighborhood idiot",
+	"over 3000 lives",
+	"forgot a semicolon",
+	"used css, and lived",
+	"has a potato stuck in his eye",
+	"will finish his first project in 2197",
+	"makes simple things like growls",
+	"is a best guy thing",
+	".php.zip.html.flv.rar.7z.png.jade.exe.gif.mov.js",
+	"dududududude!",
+	"20% blockier",
+	"really loves spoons",
+	"apples are cool",
+	"it's a type of cat",
+	"likes to smash faec in coocky",
+	"ae",
+	"really hates periods",
+	"thought a php file was his friend robert",
+	"doesn't care about your hair",
+	"is actually a cow",
+	"it can't crash if it never loads",
+	"squishy splatoonians",
+	"*filler splash*",
+	"not a kid or a squid",
+	"pizza is good",
+	"error 404, splash not found",
+	"likes to fly away",
+	"no u"
+];
+
+num = Math.floor(Math.random() * splashes.length);
+document.write(splashes[num]);
